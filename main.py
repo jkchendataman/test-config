c8134a91f0e63082ca6a04385297dd0bbba912ed
lhfu <lhfu@dataman-inc.com>
2016-12-20 01:16:56 +0800

    Init

---
+++ main.py
@@ -0,0 +1,23 @@
+import tornado.ioloop
+import tornado.web
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        with open("/etc/hehe/conf.ini", "r") as fp:
+            content = fp.read()
+
+            self.render('index.html', content=content)
+
+
+if __name__ == "__main__":
+    settings = {
+        'template_path': './templates',
+        'debug': True,
+    }
+    application = tornado.web.Application([
+        (r"/", MainHandler),
+    ], **settings)
+    application.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
+
